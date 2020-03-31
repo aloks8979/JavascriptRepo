7a1f22ddfb8f2478f2187e4e2c428f95d86aa34d
Alok Singh <aloksingh8979@gmail.com>
2020-03-31 23:33:52 +0530

    final checkin
    final checkin

--- Functions/exercise1SumofArguments.js
+++ Functions/exercise1SumofArguments.js
@@ -1,1 +1,27 @@
 console.log('Exercise1');
+
+function sum(...item)
+{
+    let s = 0;
+    if(Array.isArray(item) === 'true')
+    {
+        let newArr = item[0];
+        for(let i of newArr)
+            s += i;
+    }
+    else
+    {
+         for(let i of item)
+         {
+             s += i;     
+         }
+         return s;
+    }
+    return s;
+}
+
+console.log(sum([1,2,3,4]));
+
+
+
+
--- Functions/exerrcise2AreaOfcircle.js
+++ Functions/exerrcise2AreaOfcircle.js
@@ -1,1 +1,10 @@
 console.log('Exercise2');
+
+let circle = {
+    radius : 1,
+    get area(){
+        console.log('area');
+    }
+};
+
+console.log(circle.radius + '  ' + circle.area);
